2025ccb8eb12dcc8f4aef40349b3a798e0e4765c
said ouhmmou <84190116+souh03@users.noreply.github.com>
2021-09-10 13:14:28 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
-
+import SearchButton from "./components/SearchButton";
+import Header from "./components/Heading";
+import TouristInfoCards from "./components/TouristInfoCards";
+import Footer from "./components/Footer";
+//import SearchResults from "./components/SearchResults";
 import Bookings from "./components/Bookings";
 import "./App.css";
+
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">CYF Hotel</header>
+      <Header/>
+      <TouristInfoCards/>
       <Bookings />
+      
+      <Footer
+      array={[
+        "123 Fake Street, London, E1 4UD",
+        "hello@fakehotel.com",
+        "0123 456789"
+      ]}
+      />
+      
     </div>
   );
 };

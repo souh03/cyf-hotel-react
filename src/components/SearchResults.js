58d15def003dd4754df0645aa88ddd630f2e41dd
said ouhmmou <84190116+souh03@users.noreply.github.com>
2021-09-14 18:28:50 +0200

    update

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,40 +1,56 @@
 import React from "react";
+import moment from "moment";
 
-const SearchResults = (props) =>{
-    return(
-<div>
-      <table class="table">
-        <thead >
-          <tr>
-            <th scope="col">id</th>
-            <th scope="col">title</th>
-            <th scope="col">first name</th>
-            <th scope="col">surname</th>
-            <th scope="col">email</th>
-            <th scope="col">room id</th>
-            <th scope="col">check in date</th>
-            <th scope="col">check out date</th>
-          </tr>
-        </thead>
-        <tbody>
+const daysBetweenDates = (dateA, dateB) => {
+  const firstMoment = moment(dateA);
+  const secondMoment = moment(dateB);
+
+  return firstMoment.diff(secondMoment, "days");
+};
+
+const calculateBookingLength = booking => {
+  return daysBetweenDates(booking.checkOutDate, booking.checkInDate);
+};
+
+const TableRow = props => {
+  return (
+    <tr>
+      <th scope="row">{props.booking.id}</th>
+      <td>{props.booking.title}</td>
+      <td>{props.booking.firstName}</td>
+      <td>{props.booking.surname}</td>
+      <td>{props.booking.email}</td>
+      <td>{props.booking.roomId}</td>
+      <td>{props.booking.checkInDate}</td>
+      <td>{props.booking.checkOutDate}</td>
+      <td>{calculateBookingLength(props.booking)}</td>
+    </tr>
+  );
+};
+
+const SearchResults = props => {
+  return (
+    <table className="table">
+      <thead>
         <tr>
-          <tr>
-            <td scope="col">{props.id}</td>
-            <td scope="col">{props.title}</td>
-            <td scope="col">{props.firstName}</td>
-            <td scope="col">{props.surname}</td>
-            <td scope="col">{props.email}</td>
-            <td scope="col">{props.roomId}</td>
-            <td scope="col">{props.checkInDate}</td>
-            <td scope="col">{props.checkOutDate}</td>
-          </tr>
-          </tr>
-
-        </tbody>
-      </table>
-    </div>
-
-    );
+          <th scope="col">Id</th>
+          <th scope="col">Title</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Surname</th>
+          <th scope="col">Email</th>
+          <th scope="col">Room Id</th>
+          <th scope="col">Check-in</th>
+          <th scope="col">Check-out</th>
+          <th scope="col">Number of Nights</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.results.map((booking, i) => (
+          <TableRow key={i} booking={booking} />
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default SearchResults;
